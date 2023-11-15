70a3bd1d07767934dc023f0afdf5ab6a1ba002c2
Joao Vitor Tomadon Daciuk <128230606+T0madon@users.noreply.github.com>
2023-11-15 13:10:46 -0300

    Create pilha_aloc.h
    headers

---
+++ pilha_aloc.h
@@ -0,0 +1,11 @@
+#ifndef PILHA_ALOC2_H_INCLUDED
+#define PILHA_ALOC2_H_INCLUDED
+
+
+void push(int valor);
+void pop();
+void topo_pilha();
+void vazia();
+void imprime();
+
+#endif // PILHA_ALOC2_H_INCLUDED
